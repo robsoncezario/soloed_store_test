d467c2beb9ff3c2f6f316e3d8929d85ad29cc8aa
Robson <robsoncezariosilva@gmail.com>
2021-02-15 20:42:38 -0300

    Remoção do console.log

--- server/src/controllers/Item/controller.ts
+++ server/src/controllers/Item/controller.ts
@@ -9,8 +9,6 @@ export default class ItemController {
   static getManyById = async (request: any, response: any) => {
     const {items} = request.body;
 
-    console.log(items);
-
     response.status(201);
     response.json({
       products: stock.products.filter(item => items.findIndex((i: any) => i.id === item.id) !== -1)
